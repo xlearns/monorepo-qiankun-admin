96bdd003bfe67d0a3e70959d2e600f1d0407af7d
xlearns <2447684095@qq.com>
2023-02-13 22:17:27 +0800

    feat: close tailwind default style for script template

--- templates/micro-react-vite/tailwind.config.js
+++ templates/micro-react-vite/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  corePlugins: {
+    preflight: false,
+  },
+};
